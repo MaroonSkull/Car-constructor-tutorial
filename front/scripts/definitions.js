523c31d2dead65c1ee4f4f4b9a56143972988fd9
Gregory Kashkin <33577562+MaroonSkull@users.noreply.github.com>
2022-07-11 08:10:00 +0300

    Изменение организации директорий

--- front/scripts/definitions.js
+++
@@ -1,6 +0,0 @@
-// настройка сервопривода, ось x
-const servoMin = 15, servoMax = 189;
-let servoAngle = (servoMax-servoMin)/2+servoMin;
-
-// настройка скорости, ось y
-let speedMin = -100, speedMax = 100;
